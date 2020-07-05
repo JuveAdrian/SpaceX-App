44376b7e9eb07ecf0aa89a90cc3b5912a4ceb6c6
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-05 23:43:30 +0200

    fetch spacex launches from api

--- client/src/components/ContentContainer.js
+++ client/src/components/ContentContainer.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class ContentContainer extends React.Component {
+    state = {
+        launches: []
+    }
+
+    componentDidMount() {
+        fetch('https://api.spacexdata.com/v4/launches')
+            .then(res => res.json())
+            .then(json => this.setState({launches : json}))
+    }
+
     render () {
         return (
-            <h1>Content Container</h1>
+            <div>
+                <ul>
+                    {this.state.launches.map(el => (
+                        <li>
+                            <h2>{el.name}</h2>
+                            <img src={el.links.patch.small} alt="{el.links.patch.small}"></img>
+
+                        </li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
