110239bd95ef1273062f4eaa89dc5de25cd6347b
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-09 23:57:40 +0200

    workign on

--- client/src/pages/launche.js
+++ client/src/pages/launche.js
@@ -14,31 +14,45 @@ class Launche extends React.Component {
         }
 
         componentDidMount() {
-            this.setState({loading: true})
-            console.log(this.state.id)
+            this.setState({loading: true});
             fetch(`https://api.spacexdata.com/v4/launches/${this.state.id}`)
-                .then( () => {
-                    this.setState({ loading: false})
-                    console.log(true)
-                })
-                .then(res => res.json())
-                .then(json => this.setState({ data : json }))
-                .then((json)=> console.log('fetched'))
-                .catch(err => {
-                    this.setState({loading: false})
-                    this.setState({ hasError: true })
-                })
-                }
+            .then(res => res.json())
+            .then(json => {
+                this.setState({...this.state, data: json })
+                this.setLoading({ loading: false})
+            })
+            .catch(err => {
+                this.setState({loading: false})
+                this.setState({ hasError: true })            
+            })
+        }
+        // componentDidMount() {
+        //     this.setState({loading: true})
+        //     console.log(this.state.id)
+        //     fetch(`https://api.spacexdata.com/v4/launches/${this.state.id}`)
+        //         .then( () => {
+        //             this.setState({ loading: false})
+        //             console.log(true)
+        //         })
+        //         .then(res => res.json())
+        //         .then(json => this.setState({ data : json }))
+        //         .then((json)=> console.log('fetched'))
+        //         .catch(err => {
+        //             this.setState({loading: false})
+        //             this.setState({ hasError: true })
+        //     })
+        // }
         
-
     
 
     render () {
-        console.log(this.state.data);
+        console.log(this.state.data)
         return (
             <>
                 <Menu />
-                { this.state.loading ? <div>loading...</div> : this.state.hasError ? <div>Error occured fetching data</div> : console.log(this.state.data) }
+                { this.state.loading ? <h1>loading...</h1> : this.state.hasError ? <div>Error occured fetching data</div> : <h1>{ this.state.data.map(el => el.name)}</h1> }
+
+                {/* { this.state.loading ? <div>loading...</div> : this.state.hasError ? <div>Error occured fetching data</div> : console.log(this.state.data) } */}
             </>
         )
     }
