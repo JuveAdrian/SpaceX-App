e1e7b724307ed095b9015cb9c3e187d091d1f1db
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-09 13:16:21 +0200

    try Fetch single launche data withi Loading... text

--- client/src/pages/launche.js
+++ client/src/pages/launche.js
@@ -3,25 +3,43 @@ import React from 'react';
 import Menu from '../components/Menu';
 
 class Launche extends React.Component {
-        state = {
-            id: this.props.match.params.id,
-            launche: []
+        constructor(props) {
+            super();
+            this.state = {
+                id: props.match.params.id,
+                data: [],
+                hasError: false,
+                loading: false
+            }
         }
-    
-    
-        useEffect() {
+
+        componentDidMount() {
+            this.setState({loading: true})
+            console.log(this.state.id)
             fetch(`https://api.spacexdata.com/v4/launches/${this.state.id}`)
-                .then(res => this.setState({ launche: res.data }));
-        }
-   
+                .then( () => {
+                    this.setState({ loading: false})
+                    console.log(true)
+                })
+                .then(res => res.json())
+                .then(json => this.setState({ data : json }))
+                .then((json)=> console.log('fetched'))
+                .catch(err => {
+                    this.setState({loading: false})
+                    this.setState({ hasError: true })
+                })
+                }
+        
+
+    
 
     render () {
-        console.log(this.state.launche);
+        console.log(this.state.data);
         return (
-            <div>
-                 <Menu />
-                 <h1>{this.state.launche.name}</h1>
-            </div>
+            <>
+                <Menu />
+                { this.state.loading ? <div>loading...</div> : this.state.hasError ? <div>Error occured fetching data</div> : console.log(this.state.data) }
+            </>
         )
     }
 }
