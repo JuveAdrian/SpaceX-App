7222d9977682f3f36b2bafaa1ddb3b337f7e9aac
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-10 23:41:40 +0200

    try fix isuee

--- client/src/pages/launche.js
+++ client/src/pages/launche.js
@@ -3,61 +3,35 @@ import Async from 'react-async';
 
 import Menu from '../components/Menu';
 
+const loadPosts = () => 
+    fetch('https://api.spacexdata.com/v4/launches/5eb87cdaffd86e000604b32b')
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
 
-class Launche extends React.Component {
-        constructor(props) {
-            super();
-            this.state = {
-                id: props.match.params.id,
-                data: [],
-                hasError: false,
-                loading: false
+const Launche = () => (
+    <Async promiseFn={loadPosts}>
+        {({ data, error, isLoading }) => {
+            if (isLoading) return <div>Fetching data... Please Wait</div>
+
+            if (error) return <div>Something went wrong: ${error.message}</div>
+
+            if (data){
+                const responseData = (data) => {
+                    return(
+                        <div>
+                            <p>{data.name}</p>
+                        </div>
+                    )
+                }
+            return(
+                <div>
+                    {responseData}
+                </div>
+            )    
             }
-        }
-
-        componentDidMount() {
-            this.setState({loading: true});
-            fetch(`https://api.spacexdata.com/v4/launches/${this.state.id}`)
-            .then(res => res.json())
-            .then(json => {
-                this.setState({...this.state, data: json })
-                this.setLoading({ loading: false})
-            })
-            .catch(err => {
-                this.setState({loading: false})
-                this.setState({ hasError: true })            
-            })
-        }
-        // componentDidMount() {
-        //     this.setState({loading: true})
-        //     console.log(this.state.id)
-        //     fetch(`https://api.spacexdata.com/v4/launches/${this.state.id}`)
-        //         .then( () => {
-        //             this.setState({ loading: false})
-        //             console.log(true)
-        //         })
-        //         .then(res => res.json())
-        //         .then(json => this.setState({ data : json }))
-        //         .then((json)=> console.log('fetched'))
-        //         .catch(err => {
-        //             this.setState({loading: false})
-        //             this.setState({ hasError: true })
-        //     })
-        // }
-        
-    
-
-    render () {
-        console.log(this.state.data)
-        return (
-            <>
-                <Menu />
-                { this.state.loading ? <h1>loading...</h1> : this.state.hasError ? <div>Error occured fetching data</div> : <h1>{ this.state.data.map(el => el.name)}</h1> }
-
-                {/* { this.state.loading ? <div>loading...</div> : this.state.hasError ? <div>Error occured fetching data</div> : console.log(this.state.data) } */}
-            </>
-        )
-    }
-}
+            return null
+        }}
+    </Async>
+)
 
 export default Launche;
