55661b127dbfb61f8d172765713ecb5ab46bba4d
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-12 00:40:59 +0200

    fetching crew

--- client/src/pages/crew.js
+++ client/src/pages/crew.js
@@ -3,11 +3,29 @@ import React from 'react';
 import Menu from '../components/Menu';
 
 class Crew extends React.Component {
+    state = {
+        crew: []
+    }
+
+    componentDidMount() {
+        fetch('https://api.spacexdata.com/v4/crew')
+            .then(res => res.json())
+            .then(json => this.setState({crew : json}));
+    }
+
     render () {
         return (
             <div>
                 <Menu />
                 <h1>SpaceX Crew</h1>
+                {this.state.crew.map( el => (
+                    <div>
+                        <p>{ el.name }</p>
+                        <img src={el.image} alt={el.name} />
+                    </div>
+                    
+                ))}
+                
             </div>
         )
     }
