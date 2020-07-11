eb99fdb5f99437c4af1da7164cfc77c0a092d656
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-12 00:58:15 +0200

    simple styling crew page

--- client/src/pages/crew.js
+++ client/src/pages/crew.js
@@ -17,14 +17,18 @@ class Crew extends React.Component {
         return (
             <div>
                 <Menu />
+               
+                <div className="crewContainer">
                 <h1>SpaceX Crew</h1>
-                {this.state.crew.map( el => (
-                    <div>
-                        <p>{ el.name }</p>
-                        <img src={el.image} alt={el.name} />
-                    </div>
-                    
-                ))}
+                    {this.state.crew.map( el => (
+                        <div className="crewMember">
+                            <h1>{ el.name }</h1>
+                            <img src={el.image} alt={el.name} />
+                        </div>
+                        
+                    ))}
+                </div>
+                
                 
             </div>
         )
