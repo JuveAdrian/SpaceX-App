e25cc71d0c9ffa0af68423842a5d7b2fc52bc00c
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-16 22:11:12 +0200

    save

--- client/src/pages/launches.js
+++ client/src/pages/launches.js
@@ -18,29 +18,33 @@ class Launches extends React.Component {
 
     render () {
         return (
-            <main>
-                <div className="data-container-header">
-                    <h1>{this.state.pageName}</h1>
-                </div>
-                {this.state.launches.map(el => (
-                <div className="item-container" key="el.id">
-                    <div className="image"><a href="#"><img src={el.links.patch.small} /></a></div>
-                    <div className="describe">
-                        <a href="#"><h1 className="name">{el.name}</h1></a>
-                        <Link to={{ 
-                            pathname: `/launche/${el.id}`, 
-                            state: { 
-                                name: el.name,
-                                img: el.links.patch.small,
-                                gallery: el.links.flickr.original }
-                        }}>
-                            <button>Read More</button>
-                        </Link>
-                        <span>{el.date}</span>
+            <div>
+                <Menu />
+                <main>
+                    <div className="data-container-header">
+                        <h1>{this.state.pageName}</h1>
                     </div>
-                </div>
-                ))}
-            </main>
+                    {this.state.launches.map(el => (
+                    <div className="item-container" key="el.id">
+                        <div className="image"><a href="#"><img src={el.links.patch.small} /></a></div>
+                        <div className="describe">
+                            <a href="#"><h1 className="name">{el.name}</h1></a>
+                            <Link to={{ 
+                                pathname: `/launche/${el.id}`, 
+                                state: { 
+                                    name: el.name,
+                                    img: el.links.patch.small,
+                                    gallery: el.links.flickr.original }
+                            }}>
+                                <button>Read More</button>
+                            </Link>
+                            <span>{el.date}</span>
+                        </div>
+                    </div>
+                    ))}
+                </main>
+            </div>
+            
         )
     }
 }
