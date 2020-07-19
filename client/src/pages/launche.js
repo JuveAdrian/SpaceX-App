bfa0070212e67a02f27a9281f870a2e329e06625
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-20 00:01:56 +0200

    success/failed mision status

--- client/src/pages/launche.js
+++ client/src/pages/launche.js
@@ -3,6 +3,31 @@ import React from 'react';
 import Menu from '../components/Menu';
 
 class Launche extends React.Component {
+    state = {
+        id: this.props.location.state.launchpad,
+        launchpad: [],
+        status: '',
+        color: ''
+    }
+
+    componentDidMount() {
+        if(this.props.location.state.success) {
+            this.setState({status : 'Success'})
+        } else {
+            this.setState({status : 'Failed', color: '#FF3232'})
+        }
+
+        console.log(this.props.location.state.launchpad);
+        console.log(this.state.launchpad);
+        fetch(`https://api.spacexdata.com/v4/launchpads/${this.state.id}`)
+            .then(res => res.json())
+            .then(json => this.setState({launchpad : json})).then(
+                console.log(this.state.launchpad)
+            )
+    }
+
+    com
+
     render() {
         return (
             <div>
@@ -12,7 +37,7 @@ class Launche extends React.Component {
                         <h1>{this.props.location.state.name}</h1>
                     </div>
                     <div className="single-item-container">
-                        <div className="success">{this.props.location.state.success}</div>
+                        <div className="success" style={{backgroundColor: this.state.color}}>{this.state.status}</div>
 
                         <div className="describe-group">
                             <div className="image">
@@ -25,7 +50,7 @@ class Launche extends React.Component {
                             <div className="details">
                                 <ul>
                                     <li><span>Launch Date:</span> {this.props.location.state.date}</li>
-                                    <li><span>Launch site:</span> {this.props.location.state.launchpad}</li>
+                                    <li><span>Launch site:</span></li>
                                     <li><span>Rocket:</span> {this.props.location.state.rocket}</li>
                                 </ul>
                             </div>
