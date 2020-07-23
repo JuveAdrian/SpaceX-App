e0edaae65d1873a75e99d89f8dde2783fad978ee
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-23 23:34:29 +0200

    fetch rockets

--- client/src/pages/rockets.js
+++ client/src/pages/rockets.js
@@ -3,6 +3,17 @@ import React from 'react';
 import Menu from '../components/Menu';
 
 class Rockets extends React.Component {
+    state = {
+        rockets: [],
+        pageName: 'SpaceX Rockets'
+    }
+
+    componentDidMount() {
+        fetch('https://api.spacexdata.com/v4/rockets')
+            .then(res => res.json())
+            .then(json => this.setState({rockets : json}));
+    }
+    
     render () {
         return (
             <div>
