1e639c840d298cea70696946fd53a6e369142873
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-28 21:58:55 +0200

    css fix issue

--- client/src/pages/crew.js
+++ client/src/pages/crew.js
@@ -20,39 +20,41 @@ class Crew extends React.Component {
         return (
             <div>
                 <Menu />
-                <div className="crewContainer">
-                <ContentHeader state={{ pageName: this.state.pageName }} />
-                    <div className="crewMemberBox">
-                    {this.state.crew.map( el => (
-                        <div className="crewMember">
-                            <Link to={{ 
-                                pathname: `/member/${el.id}`,
-                                state: {
-                                    wikipedia: el.wikipedia,
-                                    agency: el.agency,
-                                    status: el.status,
-                                    image: el.image,
-                                    name: el.name
-                                } 
-                            }} >
-                                <h1>{ el.name }</h1>
-                            </Link>
-                            <Link to={{ 
-                                pathname: `/member/${el.id}`,
-                                state: {
-                                    wikipedia: el.wikipedia,
-                                    agency: el.agency,
-                                    status: el.status,
-                                    image: el.image,
-                                    name: el.name
-                                } 
-                            }} >
-                                <img src={el.image} alt={el.name} />
-                            </Link>
+                <main>
+                    <div className="crewContainer">
+                    <ContentHeader state={{ pageName: this.state.pageName }} />
+                        <div className="crewMemberBox">
+                        {this.state.crew.map( el => (
+                            <div className="crewMember">
+                                <Link to={{ 
+                                    pathname: `/member/${el.id}`,
+                                    state: {
+                                        wikipedia: el.wikipedia,
+                                        agency: el.agency,
+                                        status: el.status,
+                                        image: el.image,
+                                        name: el.name
+                                    } 
+                                }} >
+                                    <h1>{ el.name }</h1>
+                                </Link>
+                                <Link to={{ 
+                                    pathname: `/member/${el.id}`,
+                                    state: {
+                                        wikipedia: el.wikipedia,
+                                        agency: el.agency,
+                                        status: el.status,
+                                        image: el.image,
+                                        name: el.name
+                                    } 
+                                }} >
+                                    <img src={el.image} alt={el.name} />
+                                </Link>
+                            </div>
+                        ))}
                         </div>
-                    ))}
                     </div>
-                </div>
+                </main>
             </div>
         )
     }
