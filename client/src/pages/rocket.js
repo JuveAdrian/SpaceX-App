772b1d1df3be79cb10f6a84f6ad113baea8d7aef
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-07-29 23:01:08 +0200

    display rocket data

--- client/src/pages/rocket.js
+++ client/src/pages/rocket.js
@@ -10,6 +10,16 @@ class Rocket extends React.Component {
                 <Menu />
                 <main>
                     <ContentHeader state={{ pageName: this.props.location.state.name }}/>
+                    <div className="rockets-list">
+                        <img src={this.props.location.state.photos[0]} alt={this.props.location.state.name} />
+                        <ul>
+                            <li>Name: {this.props.location.state.name}</li>
+                            <li>Description: {this.props.location.state.description}</li>
+                            <li>Height: {this.props.location.state.height.meters}m</li>
+                            <li>First Flight: {this.props.location.state.firstFlight}</li>
+                            <li>Mass: {this.props.location.state.mass}kg</li>
+                        </ul>
+                    </div>
                 </main>
             </div>
         )
