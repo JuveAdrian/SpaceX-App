423ecc5b7e404eb11058aceda52263f423f1857f
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-08-05 23:48:03 +0200

    save

--- client/src/pages/launchesNew.js
+++ client/src/pages/launchesNew.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import { Grid, makeStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 
 const useStyles = makeStyles({
@@ -16,8 +22,7 @@ const useStyles = makeStyles({
     },
     main: {
         justifyContent: 'center',
-        height: '1000px',
-        backgroundColor: 'green',
+        backgroundColor: 'green'
     },
     content: {
         backgroundColor: 'red'
@@ -35,7 +40,78 @@ const LaunchesNew = ()  =>  {
                     <Grid item xs={3}>Search</Grid>
                 </Grid>
                 <Grid item xs={10} className={classes.main} container>
-                    <Grid item xs={12} className={classes.content}>
+                    <Grid item xs={12} className={classes.content} spacing={3} container>
+                        <Grid item xs={4}>
+                            <Card className={classes.root} >
+                                <CardContent>
+                                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Word of the Day
+                                    </Typography>
+                                    <Typography variant="h5" component="h2">
+                                    be-nev-o-lent
+                                    </Typography>
+                                    <Typography className={classes.pos} color="textSecondary">
+                                    adjective
+                                    </Typography>
+                                    <Typography variant="body2" component="p">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                        
+                        <Grid item xs={4}>
+                            <Card className={classes.root} >
+                                <CardContent>
+                                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Word of the Day
+                                    </Typography>
+                                    <Typography variant="h5" component="h2">
+                                    be-nev-o-lent
+                                    </Typography>
+                                    <Typography className={classes.pos} color="textSecondary">
+                                    adjective
+                                    </Typography>
+                                    <Typography variant="body2" component="p">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <Card className={classes.root} >
+                                <CardContent>
+                                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    Word of the Day
+                                    </Typography>
+                                    <Typography variant="h5" component="h2">
+                                    be-nev-o-lent
+                                    </Typography>
+                                    <Typography className={classes.pos} color="textSecondary">
+                                    adjective
+                                    </Typography>
+                                    <Typography variant="body2" component="p">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
