6ec1d5788b85258f9efb7e6de1ee9c41ed6f8147
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-08-15 23:25:21 +0200

    delete old html template and App.css

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-/* import './App.css'; */
+import './Appv2.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Menu from '../components/Menu';
 import Navbar from '../components/Navbar';
 import ContentContainer from '../components/ContentContainer';
 
