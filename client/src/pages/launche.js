4b16de0938125259f2e8a37115a71749422bf1bc
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-08-21 15:15:05 +0200

    save

--- client/src/pages/launche.js
+++ client/src/pages/launche.js
@@ -11,7 +11,6 @@ import CardHeader from '@material-ui/core/CardHeader';
 import { ThemeProvider } from '@material-ui/styles';
 import { GridList, GridListTile, ListSubheader } from '@material-ui/core';
 import { Divider } from '@material-ui/core';
-import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = theme => makeStyles({
@@ -62,22 +61,22 @@ class Launche extends React.Component {
                                     <CardHeader title={this.props.location.state.name} style={{ color: '#fff', textAlign: 'center' }} />
                                     <Divider />
                                     <Grid item container xs={12} style={{ backgroundColor: '#455a64' }}>
-                                        <Grid item xs={6} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                        <Grid item xs={12} sm={6} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                                             <img src={this.props.location.state.img} alt={'Launch Patch'} style={{ height: '300px', padding: '20px 0' }} />
                                         </Grid>
-                                        <Grid item xs={6} style={{ padding: '0 15px', color: '#eee', paddingBottom: '20px' }}>
-                                            <MuiAlert severity="success">This is a success message!</MuiAlert>
-                                            <h3 style={{ lineHeight: '2em', color: '#bbb' }}>Launch Description</h3>
-                                            <p style={{ textAlign: 'justify' }}>{this.props.location.state.details}</p>
+                                        <Grid item xs={12} sm={6} style={{ padding: '0 15px', color: '#eee', padding: '20px 0' }}>
+                                            { this.props.location.state.success === true ? <MuiAlert variant="filled" color="success" severity="success" style={{ margin: '20px' }}>Success</MuiAlert> : <MuiAlert variant="filled" color="error" severity="error" style={{ marginRight: '20px' }}>Failed</MuiAlert> }
+                                            <h3 style={{ lineHeight: '2em', color: '#bbb', padding: '0 20px', paddingTop: '20px' }}>Launch Description</h3>
+                                            <p style={{ textAlign: 'justify', padding: '0 20px' }}>{this.props.location.state.details}</p>
                                             <Divider style={{ marginTop: '15px' }} />
-                                            <h3 style={{ lineHeight: '2em', color: '#bbb' }}>Launch Date:</h3>
-                                            <p>{moment(this.props.location.state.date).format('MMMM D, YYYY')}</p>
-                                            <Divider style={{ marginTop: '15px' }} />
-                                            <h3 style={{ lineHeight: '2em', color: '#bbb' }}>Launch site:</h3>
-                                            <p>{this.state.launchpad}</p>
-                                            <Divider style={{ marginTop: '15px' }} />
-                                            <h3 style={{ lineHeight: '2em', color: '#bbb' }}>Rocket:</h3>
-                                            <p>{this.state.rocket}</p>
+                                            <h3 style={{ lineHeight: '2em', color: '#bbb', padding: '0 20px' }}>Launch Date:</h3>
+                                            <p style={{ padding: '0 20px' }}>{moment(this.props.location.state.date).format('MMMM D, YYYY')}</p>
+                                            <Divider style={{ marginTop: '15px', padding: '0 20px' }} />
+                                            <h3 style={{ lineHeight: '2em', color: '#bbb', padding: '0 20px' }}>Launch site:</h3>
+                                            <p style={{ padding: '0 20px' }}> {this.state.launchpad}</p>
+                                            <Divider style={{ marginTop: '15px', padding: '0 20px' }} />
+                                            <h3 style={{ lineHeight: '2em', color: '#bbb', padding: '0 20px' }}>Rocket:</h3>
+                                            <p style={{ padding: '0 20px' }}>{this.state.rocket}</p>
                                         </Grid>
                                     </Grid>
 
